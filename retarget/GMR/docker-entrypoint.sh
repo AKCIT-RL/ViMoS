dff387b9e1d331216ae9670455072800d8e46748
Gabriel_Pettro <gabrielruotolo@discente.ufg.br>
2026-04-06 17:49:53 -0300

    register retarget/GMR as submodule (was tracked as tree)

--- retarget/GMR/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Inicia Xvfb em background para fornecer display virtual ao GLFW.
-# mjv.launch_passive() precisa de X11 independente de MUJOCO_GL.
-Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX &
-export DISPLAY=:99
-exec "$@"
